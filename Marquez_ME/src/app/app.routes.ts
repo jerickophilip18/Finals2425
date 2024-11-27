90ee8e9a2ab38cc71a20c61751600e5b6cf3788a
jerickophilip18 <202174321@s.ubaguio.edu>
2024-11-27 13:40:32 +0800

    nov 27

--- Marquez_ME/src/app/app.routes.ts
+++ Marquez_ME/src/app/app.routes.ts
@@ -3,16 +3,18 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './registration/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'about', component: AboutComponent},
     { path: 'services', component: ServicesComponent},
     { path: 'contact', component: ContactComponent},
-    { path: 'login', component: LoginComponent},
+    { path: 'registration', component: LoginComponent},
+    { path: 'profile', component: ProfileComponent},
     { path: '**', component: NotFoundComponent},
 
 ];
